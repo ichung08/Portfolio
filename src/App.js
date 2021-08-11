7d44951d8c833a2de6004c91e0d4c5bac3eecf6a
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-08-11 02:11:41 -0700

    added responsive window resizing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="container">
         <About />
         <Experience />
+        <Projects />
       </div>
     </div>
   );
