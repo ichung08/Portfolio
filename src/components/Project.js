c9b30dcb27bc9abb2d90d1232378b57a0dffd64d
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-08-13 18:24:03 -0700

    updated project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,6 +15,8 @@ const Project = (props) => {
     setRotateState(setActive === "active" ? "accordion__icon" : "accordion__icon rotate");
   };
 
+  const techArr = props.tech.split(" ");
+
   return (
     <div className="accordion__section">
       <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
@@ -22,9 +24,21 @@ const Project = (props) => {
         <FaChevronDown className={`${setRotate}`} />
       </button>
       <div ref={content} style={{ maxHeight: `${setHeight}` }} className="accordion__content">
-        <div className="accordion__text">{props.event}</div>
+        <div className="accordion__date">{props.event}</div>
         <div className="accordion__text">{props.desc}</div>
-        <div className="accordion__text">{props.tech}</div>
+        <br />
+        {props.tech && (
+          <div className="accordion__tech__container">
+            {" "}
+            {techArr.map(function (tech, i) {
+              return (
+                <span className="accordion__tech" key={i}>
+                  {tech}
+                </span>
+              );
+            })}
+          </div>
+        )}
         {props.github && <FaGithub href={props.github} />}
         {props.website && <FaExternalLinkAlt href={props.website} />}
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,15 +11,15 @@ const Projects = () => {
         website="https://ubc-course-app.herokuapp.com/"
         event="Oakhacks 2020"
         desc="Built a school course search web app with Python and Django, integrating a UBC Grades and RateMyProfessors API to display course, grades and professor information. Containerized and deployed using Docker and Heroku and established CI/CD pipeline with Github Actions. Designed and implemented frontend web pages using Django, HTML and CSS. Achieved first place amongst 40+ teams at Oakhacks 2020."
-        tech="Python, Django, JavaScript, Heroku, Docker, Github Actions, HTML, CSS"
+        tech="Python Django JavaScript Heroku Docker GithubActions HTML CSS"
       />
       <br></br>
       <Project
         title="Restaurant List"
         github="https://github.com/ichung08/Restaurant-List-App"
-        event="independent project"
+        event="Independent Project"
         desc="Created an application to help users record and list restaurants utilizing OOP concepts. Developed desktop application in Java, with SwingUI GUI. Aquired 100% code coverage with JUnit."
-        tech="Java, SwingUI, JUnit, JSON"
+        tech="Java SwingUI JUnit JSON"
       />
       <br></br>
       <Project
@@ -27,15 +27,15 @@ const Projects = () => {
         github="https://github.com/ichung08/battle-todo"
         event="nwHacks 2021"
         desc="Developed a user authenticated to-do list game web app to encourage online productivity. Built login system, to-do list, and frontend web pages using Django, HTML and CSS. Achieved ‘Honourable Mention’ award amongst 200+ teams at nwHacks 2021."
-        tech="Python, Django, HTML, CSS"
+        tech="Python Django HTML CSS"
       />
       <br></br>
       <Project
         title="Modelling Meteorological Factors of COVID-19"
         github="https://github.com/ericlee0920/2021-Data-Science-Projects"
-        event="research project"
+        event="Research Project"
         desc="Evaluated the significance of meterological factors on COVID-19 infection rates in various regions of Canada using linear regression, decision tree regression, and a random forest regressor in Python. Presented project at Multidisciplinary Undergraduate Research Conference (MURC)."
-        tech="Python, scikit-learn, Django, MongoDB"
+        tech="Python scikit-learn Django MongoDB"
       />
     </div>
   );
